e33317483c0f9c7c1aef69581db7a5d8f2ffa298
Adam Letenyei <letenyeidam89@gmail.com>
2019-11-28 20:01:11 +0100

    Number fit

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class Anagrams {
     static boolean isAnagram(String a, String b) {
         // Complete the function
+        boolean flag = false;
         if(a.length() != b.length())return false;
         else{
+            char[] aChars = a.toCharArray();
+            char[] bChars = a.toCharArray();
+            int szamlalo = 0;
 
-            return true;
+            for (int i = 0; i < aChars.length; i++){
+
+            }
+
         }
+        return flag;
     }
 
     public static void main(String[] args) {
